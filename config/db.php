3653fd3cfd9978e319f1d7780ea54b132f56d233
Bhaskar Bhakt <bhaskarbhakt10@gmail.com>
2025-05-14 11:14:23 +0530

    adding env

--- config/db.php
+++ config/db.php
@@ -1,11 +1,16 @@
 <?php
-$host = 'localhost';
-$db   = 'daily-planner';
-$user = 'root';
-$pass = '';
+require_once dirname(__DIR__,1) . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__,1));
+$dotenv->load();
+
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
